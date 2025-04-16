a4b8b18ba83841b3fb5717f555ca704fadf37f03
Martti Mustonen <s4muma03@students.osao.fi>
2025-04-16 13:57:33 +0300

    Added code for search city and weather info (api)

--- WeatherApp/Form1.Designer.cs
+++ WeatherApp/Form1.Designer.cs
@@ -28,13 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.picWeather = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picWeather)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtCity
+            // 
+            this.txtCity.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.txtCity.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtCity.Location = new System.Drawing.Point(62, 239);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(206, 20);
+            this.txtCity.TabIndex = 0;
+            this.txtCity.Text = "Enter City";
+            this.txtCity.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lblResult
+            // 
+            this.lblResult.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.lblResult.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblResult.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.lblResult.Location = new System.Drawing.Point(62, 153);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(120, 80);
+            this.lblResult.TabIndex = 2;
+            this.lblResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblResult.Click += new System.EventHandler(this.lblResult_Click);
+            // 
+            // picWeather
+            // 
+            this.picWeather.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.picWeather.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picWeather.Location = new System.Drawing.Point(188, 153);
+            this.picWeather.Name = "picWeather";
+            this.picWeather.Size = new System.Drawing.Size(80, 80);
+            this.picWeather.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picWeather.TabIndex = 3;
+            this.picWeather.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(367, 425);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.picWeather);
+            this.Controls.Add(this.txtCity);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picWeather)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.PictureBox picWeather;
     }
 }
 
--- WeatherApp/Form1.cs
+++ WeatherApp/Form1.cs
@@ -1,20 +1,81 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net.Http;
 using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
 
 namespace WeatherApp
 {
     public partial class Form1 : Form
     {
+        private static readonly HttpClient client = new HttpClient();
+        private string apiKey = "dd576a76eae194a3009cfd364f1b49d4";
+
         public Form1()
         {
             InitializeComponent();
+
+            txtCity.KeyDown += TxtCity_KeyDown;
+            txtCity.PreviewKeyDown += TxtCity_PreviewKeyDown;
+        }
+
+        private void TxtCity_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                e.IsInputKey = true;
+        }
+
+        private async void TxtCity_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.SuppressKeyPress = true;
+
+                string city = txtCity.Text.Trim();
+                city = System.Globalization.CultureInfo.CurrentCulture.TextInfo.ToTitleCase(city.ToLower());
+
+                if (string.IsNullOrWhiteSpace(city))
+                {
+                    lblResult.Text = "Please enter a city name.";
+                    return;
+                }
+
+                string url = $"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}&units=metric";
+
+                try
+                {
+                    string response = await client.GetStringAsync(url);
+                    JObject data = JObject.Parse(response);
+
+                    string description = data["weather"][0]["description"].ToString();
+                    string temp = data["main"]["temp"].ToString();
+                    string humidity = data["main"]["humidity"].ToString();
+                    string icon = data["weather"][0]["icon"].ToString();
+                    string wind = data["wind"]["speed"].ToString();
+
+                    lblResult.Text =
+                        $"Weather in {city}:\n" +
+                        $"{description}\n" +
+                        $"Temp: {temp}°C\n" +
+                        $"Humidity: {humidity}%\n" +
+                        $"Wind: {wind} m/s";
+
+                    string iconUrl = $"https://openweathermap.org/img/wn/{icon}@2x.png";
+                    picWeather.Load(iconUrl);
+                }
+                catch (Exception ex)
+                {
+                    lblResult.Text = "Error: " + ex.Message;
+                    picWeather.Image = null;
+                }
+
+                txtCity.Clear();
+                txtCity.Focus();
+            }
+        }
+
+        private void lblResult_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
